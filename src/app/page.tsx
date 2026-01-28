d8f0247816dcc5dda78e44415a17f66899c424aa
tahmid122 <mdtahmidalam122@gmail.com>
2026-01-28 09:12:31 +0600

    (component) added DatePicker (src/components/custom/reuseable)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ const Home = () => {
   return (
     <div>
       <h1 className="text-center text-3xl font-semibold">Index page</h1>
+      {/* Test div */}
+      <div></div>
     </div>
   );
 };
---
+++ src/components/custom/reuseable/DatePicker.tsx
@@ -0,0 +1,46 @@
+// ! Instruction (Remove this)
+// * From parent component pass date, setDate
+// *  const [date, setDate] = useState<Date>();
+// * <DatePicker date={date} setDate={setDate} />
+// * After change you will get the value to date state
+
+"use client";
+
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { ChevronDownIcon } from "lucide-react";
+interface Props {
+  date: Date | undefined;
+  setDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
+}
+export function DatePicker({ date, setDate }: Props) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          data-empty={!date}
+          className="data-[empty=true]:text-muted-foreground w-[212px] justify-between text-left font-normal"
+        >
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          <ChevronDownIcon />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          defaultMonth={date}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
