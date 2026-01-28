0821b479d4a0954150a89e3aac1bde0a6054cdf6
tahmid122 <mdtahmidalam122@gmail.com>
2026-01-28 12:35:31 +0600

    Update tooltip class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <h1 className="text-center text-3xl font-semibold">Index page</h1>
       {/* Test div */}
       <div className="p-5">
-        <CustomTooltip text="Custom Tooltip" side="right">
+        <CustomTooltip text="Custom Tooltip">
           <span>Hover for details</span>
         </CustomTooltip>
       </div>
