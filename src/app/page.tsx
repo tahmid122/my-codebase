22bddab2f8a5a47ddd480b14d17129c878d62974
tahmid122 <mdtahmidalam122@gmail.com>
2026-01-28 11:43:46 +0600

    (components) added DynamicPagination (src/components/custom/reuseable)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { DynamicPagination } from "@/components/custom/reuseable/DynamicPagination";
+
 const Home = () => {
   return (
     <div>
       <h1 className="text-center text-3xl font-semibold">Index page</h1>
       {/* Test div */}
-      <div></div>
+      <div className="p-5"></div>
     </div>
   );
 };
